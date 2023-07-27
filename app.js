0ee730660ad4e10b86cddc34501db927ab7745e7
vish292 <98393275+vish292@users.noreply.github.com>
2023-07-27 13:55:07 +0530

    SopeNow ApexChart Task

---
+++ app.js
@@ -0,0 +1,28 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Sample data for the chart
+//     const chartData = {
+//       categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
+//       series: [44, 55, 41, 17, 15],
+//     };
+  
+//     // Options for the chart
+//     const chartOptions = {
+//       chart: {
+//         type: 'bar',
+//       },
+//       plotOptions: {
+//         bar: {
+//           horizontal: false,
+//         }
+//       },
+//       xaxis: {
+//         categories: chartData.categories,
+//       },
+//     };
+  
+//     // Create the chart
+//     const chart = new ApexCharts(document.querySelector("#chart"), chartOptions);
+  
+//     // Render the chart
+//     chart.render();
+//   });
